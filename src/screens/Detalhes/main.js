688390c84d67eb621e73f8dbe5b4b38d674b85fc
Eric Kaynan <erickaynan11@gmail.com>
2020-08-13 22:05:45 -0300

    Duas telas
    Tela de Adicionar Serviços e tela Serviços Empresas

--- src/screens/Detalhes/main.js
+++ src/screens/Detalhes/main.js
@@ -1,124 +1,237 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
+
+
 export default StyleSheet.create({
     container: {
         flex: 1,
-        alignItems:"center",
-        paddingHorizontal:10
+        backgroundColor: '#70A5FF',
     },
-    perfil:{
-       backgroundColor:"#ffff",
-       width:400,
-       height:140,
-       paddingHorizontal:20,
-       flexDirection:"row",
-       shadowColor:"#000",
-       alignItems:"center",
-       justifyContent:"center"
-           
+    gradiente: {
+        left: 0,
+        right: 0,
+        top: 0,
     },
-    logoEmpresa:{
-        width:90,
-        height:90,
-        backgroundColor:"#f3f3f3",
-        borderRadius:60,
+    header: {
+        marginHorizontal: 25,
+        flexDirection: 'row',
+        justifyContent: "center",
+        alignItems: 'center'
+
     },
-    infos:{
-        margin:0,
-        padding:0,
-        marginHorizontal:20
+    informacoes: {
+        flexDirection: 'row',
+        right: 35,
+        bottom: 15,
+        width: 250,
+        // justifyContent: "center",
+        alignItems: 'center'
     },
-    nomeEmpresa:{
-        fontFamily: "Baloo Thambi 2 Bold",
-        overflow: "visible",
-        color: "#535353",
-        fontSize: 22,
-        letterSpacing: 0,
+    introHeader: {
+        justifyContent: 'center',
+        height: 179,
+        marginLeft: 8
     },
-    categoria:{
-        fontFamily: "Baloo Thambi 2 Regular",
-        overflow: "visible",
-        color: "#535353",
-        letterSpacing: 0,
-        fontSize:16,
-    },
-    infosCategStar:{
-        flexDirection:"row",
-        alignItems:"center"
-    },
-    star:{
-        flexDirection:"row",
-        justifyContent:"space-between"
-    },
-    numberStar:{
-        fontFamily: "Baloo Thambi 2 Bold",
-        overflow: "visible",
-        color: "#FFD765",
-        fontSize: 17,
-        letterSpacing: 0,
-    },
-    icon:{
-        position:"relative",
-        top:20,
-    },
-    services:{
-      backgroundColor:"#ffff",
-      marginTop:12,
-      flex:1,
-      width:400,
-      paddingHorizontal:30
-    },
-    headerServices:{
-        marginTop:20,
-        fontFamily: "Baloo Thambi 2 Bold",
-        overflow: "visible",
-        color: "#FFD765",
-        fontSize: 17,
-        letterSpacing: 0,
-    },
-    txtHeader:{
-        fontFamily: "Baloo Thambi 2 Bold",
-        overflow: "visible",
-        color: "#535353",
-        fontSize: 21,
-        letterSpacing: 0,
-        borderBottomColor:"#f3f3f3",
-        borderBottomWidth:1,
-        height:55
-    },
-    itemService:{
-        marginTop:20
-    },
-    tituloServiceText:{
-        fontFamily: "Baloo Thambi 2 Regular",
-        overflow: "visible",
-        color: "#4585E8",
+    textIntro: {
+        color: '#ffff',
         fontSize: 18,
+        fontFamily: 'Quicksand Bold',
         letterSpacing: 0,
-        height:30,
+        fontStyle: "normal",
+        maxWidth: 200
     },
-    descText:{
-        fontFamily: "Baloo Thambi 2 Regular",
-        overflow: "visible",
-        color: "#95979A",
-        fontSize: 14,
-        letterSpacing: 0,
-        textAlign:"left",
-    },
-    valor:{
-        height:50,
-        borderBottomColor:"#f3f3f3",
-        borderBottomWidth:1,
-    },
-    valorText:{
-        marginTop:8,
-        fontFamily: "Baloo Thambi 2 Bold",
-        overflow: "visible",
-        color: "#525252",
+    textSpan: {
+        color: '#ffff',
         fontSize: 18,
+        fontFamily: 'Quicksand Regular',
         letterSpacing: 0,
-        textAlign:"left",
+        fontStyle: "normal",
+        marginTop: 2,
+        maxWidth: 200
+    },
+    textNota: {
+        color: '#ffff',
+        fontSize: 15,
+        fontFamily: 'Quicksand Regular',
+        marginLeft: 5,
+        marginBottom: 3
+    },
+    avatarPerfil: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        backgroundColor: "#ffff",
+        marginTop: 25,
+        alignItems: "center",
+        justifyContent: "center",
+        // borderWidth: 5, 
+        // borderColor: '#c0392b'
+    },
+    avaliacao: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        position: 'absolute',
+        bottom: 10,
+        right: 0
+    },
+    content: {
         
+        backgroundColor: "#F5F5F5",
+        borderTopRightRadius: 33,
+    },
+    headerContent: {
+        marginTop: 30,
+        marginLeft: 40
+    },
+    titleHeaderContent: {
+        fontFamily: "Baloo Thambi 2 Bold",
+        color: "rgba(0, 0, 0, 0.63)",
+        fontSize: 23,
+        fontStyle: "normal",
+        letterSpacing: 0,
+        overflow: "visible",
+    },
+    categorias: {
+        marginTop: 20,
+        alignItems: "center"
+    },
+    categoriaMec: {
+        width: 87,
+        height: 90,
+        overflow: "visible",
+        backgroundColor: "#fcfcfc",
+        borderRadius: 20,
+        marginHorizontal: 28,
+    },
+    categoriaAcess: {
+        width: 87,
+        height: 90,
+        overflow: "visible",
+        backgroundColor: "#fcfcfc",
+        borderRadius: 20,
+    },
+    categoriaBorracharia: {
+        width: 87,
+        height: 90,
+        overflow: "visible",
+        backgroundColor: "#fcfcfc",
+        borderRadius: 20,
+        marginHorizontal: 28,
+    },
+    categoriaFunil: {
+        width: 87,
+        height: 90,
+        overflow: "visible",
+        backgroundColor: "#fcfcfc",
+        borderRadius: 20,
+    },
+    categoriaLavaJato: {
+        width: 87,
+        height: 90,
+        overflow: "visible",
+        backgroundColor: "#fcfcfc",
+        borderRadius: 20,
+        marginHorizontal: 28,
+    },
+    categoriaCombustivel: {
+        width: 87,
+        height: 90,
+        overflow: "visible",
+        backgroundColor: "#fcfcfc",
+        borderRadius: 20,
+    },
+    categoriaGuaragem: {
+        width: 87,
+        height: 90,
+        overflow: "visible",
+        backgroundColor: "#fcfcfc",
+        borderRadius: 20,
+        marginHorizontal: 28,
+    },
+    categoriaGuincho: {
+        width: 87,
+        height: 90,
+        overflow: "visible",
+        backgroundColor: "#fcfcfc",
+        borderRadius: 20,
+    },
+    categoriaAgendamento: {
+        width: 87,
+        height: 90,
+        overflow: "visible",
+        backgroundColor: "#fcfcfc",
+        borderRadius: 20,
+        marginHorizontal: 28,
+    },
+    company: {
+        marginTop: 15,
+        alignItems: "center",
+    },
+    touchCompany: {
+        width: 332,
+        height: 110,
+        backgroundColor: "#FCFCFC",
+        borderRadius: 14,
+        paddingHorizontal: 8,
+        justifyContent: "center",
+        marginVertical: 15,
+    },
+    infoCompany: {
+        flexDirection: "row",
+    },
+    logoCompany: {
+        width: 75,
+        height: 75,
+        borderRadius: 18,
+        backgroundColor: "rgba(235, 235, 235, 0.5)",
+    },
+    headerCardCompany: {
+        marginLeft: 15
+    },
+    titleHeaderCompany: {
+        color: "#595959",
+        fontFamily: 'Quicksand Bold',
+        fontStyle: "normal",
+        fontSize: 16,
+    },
+    spanCompany: {
+        color: "#8c8c8c",
+        fontFamily: 'Quicksand Regular',
+        height: 28
+    },
+
+    infoExtra: {
+        justifyContent: "space-around",
+    },
+    textStar: {
+        color: '#70A5FF',
+        fontFamily: 'Baloo Thambi 2 Bold',
+        fontStyle: "normal",
+        fontSize: 14,
+        marginHorizontal: 5
+    },
+    iconClock: {
+        alignItems: "center",
+        flexDirection: "row",
+        marginLeft: 20,
+    },
+    textClock: {
+        color: "#595959",
+        fontFamily: 'Baloo Thambi 2 Bold',
+        fontStyle: "normal",
+        fontSize: 14,
+        marginLeft: 5
+    },
+    botao:{
+        backgroundColor: '#70A5FF',
+        position:'absolute',
+        width: 50,
+        height: 50,
+        borderRadius: 50,
+        alignItems: 'center',
+        justifyContent:'center',
+        bottom: 25,
+        right: 25
     }
-    
-});
+
+})
